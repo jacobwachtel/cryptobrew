2b10b87a2098afaa8bf0ef55dd22eda3d3ef263b
Jacob Wachtel <58190434+jacobwachtel@users.noreply.github.com>
2022-03-27 22:19:27 -0400

    Add: Structure and Styling for Hero Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <HeroSection />
       APP
     </div>
   );
